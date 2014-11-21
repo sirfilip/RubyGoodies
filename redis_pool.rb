808cd51d56fe967ba1e909f1618071283df21185
Filip Kostovski <filip@adcombi.com>
2014-11-21 16:17:10 +0100

    Add redis thread pool

---
+++ redis_pool.rb
@@ -0,0 +1,68 @@
+require 'thread'
+require 'redis'
+
+class RedisPool 
+
+
+  def initialize(size)
+    @size = size
+    @jobs = Queue.new
+
+    @pool = Array.new(@size) do 
+      Thread.new do 
+        redis = Redis.new
+
+        catch(:exit) do 
+          loop do 
+            block = @jobs.pop
+            block.call(redis)
+          end
+        end
+      end
+    end
+
+  end
+
+  def work(&block)
+    @jobs << block
+  end
+
+  def shutdown
+    @size.times do 
+      work { throw :exit }
+    end
+
+    at_exit { @pool.map(&:join) }
+  end
+
+end
+
+
+
+
+if $0 == __FILE__ 
+
+  require 'benchmark'
+
+
+  Benchmark.bm do |x|
+  
+    x.report(:no_thread_pool) do 
+      conn = Redis.new
+      10_000.times do |i|
+        conn.set("key_#{i}", i)
+      end
+    end
+
+    x.report(:thread_pool) do 
+      pool = RedisPool.new(5)
+      10_000.times do |i|
+        pool.work do |redis|
+          redis.set("pool_key_#{i}", i)
+        end
+      end
+      pool.shutdown
+    end
+
+  end
+end
