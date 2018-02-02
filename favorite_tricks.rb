7180c99156dab259a2e7c2b262b44bb03d1a7eeb
Filip Kostovski <filip@trackconcepts.com>
2018-02-02 14:09:37 +0100

    add fetch key trick

--- favorite_tricks.rb
+++ favorite_tricks.rb
@@ -1,2 +1,5 @@
 # add context to minitest
 Minitest::Spec::DSL.send(:alias_method, :context, :describe)
+
+# call block if the key is not found
+thing.fetch(:non_existing_key) { perform your work here if the key is missing }
