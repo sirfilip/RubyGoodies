8ef4b894eccbbb62eda129b238b35925e00daee7
Filip Kostovski <filip@trackconcepts.com>
2018-02-02 15:02:10 +0100

    use fetch for hashes always

--- favorite_tricks.rb
+++ favorite_tricks.rb
@@ -3,3 +3,5 @@ Minitest::Spec::DSL.send(:alias_method, :context, :describe)
 
 # call block if the key is not found
 thing.fetch(:non_existing_key) { perform your work here if the key is missing }
+# also always use fetch and receive a KeyError if key is not found
+thing.fetch(:non_existing_key) # will raise KeyError 
